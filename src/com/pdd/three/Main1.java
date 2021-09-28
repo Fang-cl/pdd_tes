977cb56dfd951cf3ed0ca36e34c1f74f4b0345ed
Fang <fangcl@nuaa.edu.cn>
2021-09-28 10:20:00 +0800

    添加 three main1 文件

---
+++ src/com/pdd/three/Main1.java
@@ -0,0 +1,11 @@
+package com.pdd.three;
+
+/**
+ * @Author Fang
+ * @create 2021/9/28 10:16
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("tree main test");
+    }
+}
