fb0a9626d43bc88c38756632eaf9d5612f744cac
Fang <fangcl@nuaa.edu.cn>
2021-09-28 09:53:27 +0800

    修改文件

--- src/com/pdd/second/Main1.java
+++ src/com/pdd/second/Main1.java
@@ -9,22 +9,7 @@ import java.util.Scanner;
 public class Main1 {
     static int i;
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
-        int[] arr1 = arr;
-        arr1[1] = 12;
-        for (int i:arr){
-            System.out.println(i);
-        }
-        System.out.println("===========");
-        for (int i: arr1){
-            System.out.println(i);
-        }
-    }
-
-    public static int f(int i){
-        if (i == 0) return 0;
-        if (i == 1) return 1;
-        return f(i-1) + f(i-2);
+        System.out.println("测试 拉取代码");
     }
 
 }
