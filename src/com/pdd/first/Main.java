6e60f592b911b54d97077616e070301fea6f3d3d
Fang <fangcl@nuaa.edu.cn>
2021-10-18 16:45:21 +0800

    测试多态调用

--- src/com/pdd/first/Main.java
+++ src/com/pdd/first/Main.java
@@ -1,59 +1,53 @@
 package com.pdd.first;
 
-
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @Author Fang
  * @create 2021/8/22 18:51
  */
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] s = scanner.nextLine().split(" ");
-        int n = Integer.parseInt(s[0]);
-        int m = Integer.parseInt(s[1]);
-        int x = Integer.parseInt(s[2]);
+    static class Person{
+        private String name;
+        private String age;
 
-        String[] s1 = scanner.nextLine().split(" ");
-        int[] arr = Arrays.stream(s1).mapToInt(Integer::parseInt).toArray();
-        String[] s2 = scanner.nextLine().split(" ");
-        int[] arr1 = Arrays.stream(s2).mapToInt(Integer::parseInt).toArray();
-        if (n == 1){
-            for (int i = 0;i < arr1.length;i++){
-                    System.out.print(i+1 + " ");
-            }
-            return ;
-        }
-        int max = 0;
-        int pre = arr[0];
-        int h = 0;
-        for (int i = 1;i < arr.length;i++){
-            h = arr[i] - pre;
-            if (h < x && h > max){
-                max = h;
-                pre = i;
-            }
-        }
-        pre = arr[arr.length-1];
-        for (int i = arr.length-2;i >= 0;i--){
-            h = arr[i] - pre;
-            if (h < x && h > max){
-                max = h;
-                pre = i;
-            }
-        }
-        h = Math.abs(arr[0] - arr[arr.length - 1]);
-        if (h < x && h > max){
-            max = h;
+        public String getName() {
+            return name;
         }
 
-        for (int i = 0;i < arr1.length;i++){
-            if (arr1[i] > max){
-                System.out.print(i+1 + " ");
-            }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        public void setAge(String age) {
+            this.age = age;
         }
     }
+    static class Teacher extends Person{
+
+    }
+    public static void test1(Person person){
+        System.out.println(person.getName());
+    }
+    public static void test2(Teacher teacher){
+        System.out.println(teacher.getName());
+    }
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("人类");
+        Teacher t = new Teacher();
+        t.setName("许建秋");
+        test1(p);
+        test1(t);
+        System.out.println("=================");
+//        test2(p);
+//        test2(t);
+
+    }
+
 
 }
